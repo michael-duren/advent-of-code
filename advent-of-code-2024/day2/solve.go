631ee27e44524191619ace53938339f3a7ef69e1
Michael Duren <michaeld@michaelduren.com>
2025-03-10 11:57:11 -0500

    stuck on p2 d2

--- advent-of-code-2024/day2/solve.go
+++ advent-of-code-2024/day2/solve.go
@@ -3,6 +3,7 @@ package day2
 import (
 	"advent-of-code-2024/util"
 	"math"
+	"slices"
 	"strconv"
 	"strings"
 
@@ -33,50 +34,74 @@ func newMatrix(lines []string) *matrix {
 	return &matrix{levels: m}
 }
 
-func (m *matrix) isLineSafe(i int) bool {
+func (m *matrix) isLineSafe(i int) (bool, int) {
 	line := m.levels[i]
 	if len(line) < 2 {
-		return true
+		return true, 0
 	}
 
 	desc := line[0] > line[1]
 	asc := !desc
 
 	if math.Abs(float64(line[0]-line[1])) > 3 {
-		return false
+		return false, 1
 	}
 
 	for i, item := range line {
 		// there is no nextitem
 		if i+1 == len(line) {
-			return true
+			return true, i + 1
 		}
 		nextItem := line[i+1]
 		diff := math.Abs(float64(item - nextItem))
 		if diff > 3 {
-			return false
+			return false, i + 1
 		}
 		if desc && item < nextItem {
-			return false
+			return false, i + 1
 		}
 
 		if asc && item > nextItem {
-			return false
+			return false, i + 1
 		}
 
 		if item == nextItem {
-			return false
+			return false, i + 1
 		}
 	}
+	return true, i + 1
+}
+
+func tryRemoveLevel(row *[]int, level int) bool {
+	if level < 0 || level >= len(*row) {
+		return false
+	}
+	*row = slices.Delete((*row), level, level+1)
 	return true
 }
 
 func (m *matrix) countSafe() int {
 	var total int
 	for i := range m.levels {
-		safe := m.isLineSafe(i)
+		safe, _ := m.isLineSafe(i)
 		if safe {
-			total = total + 1
+			total += 1
+		}
+	}
+	return total
+}
+
+func (m *matrix) countSafeWithDampener() int {
+	var total int
+	for i := range m.levels {
+		safe, l := m.isLineSafe(i)
+		if !safe {
+			tryRemoveLevel(&m.levels[i], l)
+			safe, _ = m.isLineSafe(i)
+		}
+		log.Infof("row: %v, result: %t", &m.levels[i], safe)
+		if safe {
+			total++
 		}
 	}
 	return total
@@ -88,5 +113,7 @@ func Solve() {
 	// lines := util.ReadFileToLines("day2/example-input.txt")
 	m := newMatrix(lines)
 	totalSafe := m.countSafe()
+	totalDamperedSafe := m.countSafeWithDampener()
 	log.Infof("Total safe for part one: %d", totalSafe)
+	log.Infof("Total dampener safe for part two: %d", totalDamperedSafe)
 }
